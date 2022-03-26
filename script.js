baa28874e5c015353d87fc58690a421fcec602a5
ayakub <79369658+ayakub@users.noreply.github.com>
2022-03-27 01:53:10 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ btns.forEach( function (btn) {
         } else {
             count =0;
         }
+        if(count > 0) {
+           value.style.color = 'green';
+        }else if (count < 0) {
+            value.style.color = 'red'
+        } else if(count == 0){
+            value.style.color = 'black'
+        }
+        
         value.textContent = count;
     })
 
 
-})
+});
